f93adbf0ba7d8719c96fbab6ccd6f58b6fd94e23
Katy248 <petrovanton247@gmail.com>
2025-09-02 15:27:41 +0300

    Update dummy ID

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -15,13 +15,19 @@ func GetClients() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 		defer cancel()
-		response, err := grpc.NodesService.NodesByGroup(ctx, &connector.NodesByGroupRequest{
-			GroupId: "dummy-id",
-		})
+		response, err := grpc.NodesService.NodesByGroup(
+			ctx,
+			&connector.NodesByGroupRequest{
+				GroupId: dummyId(),
+			})
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 			return
 		}
 		c.JSON(http.StatusOK, response)
 	}
+}
+
+func dummyId() string {
+	return "00000000-0000-0000-0000-00000000000"
 }
